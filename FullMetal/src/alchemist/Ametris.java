3a896835b866b572c9903821135e55ddf575e8e0
Ashish <ashihsharmainbox1@gmail.com>
2021-08-06 15:57:38 +0530

    Creating Project

---
+++ FullMetal/src/alchemist/Ametris.java
@@ -0,0 +1,19 @@
+package alchemist;
+
+public class Ametris {
+
+	public static void main(String[] args) {
+		
+		edward();
+		alphonse();
+		
+	}
+	public static void edward() {
+		System.out.println("Hi! I have a metalarm.");
+
+	}
+	
+	public static void alphonse() {
+		System.out.println("Hi! I wear a knight armour.");
+	}
+}
